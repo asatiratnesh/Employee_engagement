d8291b4f1e762bf2afbab4d83988624ab00f8314
Ratnesh Asati <ratnesh.asati@harbingergroup.com>
2020-01-25 17:43:02 +0530

    added empl count vs job role graph

--- StaffTurnoverApp/functions.py
+++ StaffTurnoverApp/functions.py
@@ -3,41 +3,50 @@ from io import BytesIO
 import base64
 import matplotlib.pyplot as plt
 import numpy as np
+import squarify
+import pandas as pd
+
+#import squarify
 
 
-def staffTurnoverResult(reader):
+def staffTurnoverResult(emp_data):
     # ml code goed here
-    print(reader)
-    return plotChart();
+    empl_count_per_job = {}
+    job_roles = []
+    print(emp_data['JobRole'])
+    for i in emp_data['JobRole']:
+        if i in job_roles:
+            empl_count_per_job[i] = empl_count_per_job[i]+1
+        else:
+            job_roles.append(i)
+            empl_count_per_job[i] = 1
+    treemap_empl_job = plotEmplVsJob(empl_count_per_job);
+
+    return treemap_empl_job
 
 
-def plotChart():
-    pos = np.arange(10)+ 2
+def plotEmplVsJob(empl_count_per_job):
 
-    fig = plt.figure(figsize=(8, 3))
-    ax = fig.add_subplot(111)
+    df = pd.DataFrame(list(empl_count_per_job.items()), columns=['JobRole', 'EmplCount'])
+    squarify.plot(sizes=df['EmplCount'], label=df['JobRole'], alpha=.4 )
+    plt.axis('off')
 
-    ax.barh(pos, np.arange(1, 11), align='center')
-    ax.set_yticks(pos)
-    ax.set_yticklabels(('#hcsm',
-        '#ukmedlibs',
-        '#ImmunoChat',
-        '#HCLDR',
-        '#ICTD2015',
-        '#hpmglobal',
-        '#BRCA',
-        '#BCSM',
-        '#BTSM',
-        '#OTalk',),
-        fontsize=15)
-    ax.set_xticks([])
-    ax.invert_yaxis()
+    buffer = BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    image_png = buffer.getvalue()
+    buffer.close()
 
-    ax.set_xlabel('Popularity')
-    ax.set_ylabel('Hashtags')
-    ax.set_title('Hashtags')
+    treemap_empl_job = base64.b64encode(image_png)
+    treemap_empl_job = graphic.decode('utf-8')
+    return treemap_empl_job
 
-    plt.tight_layout()
+
+def plotEmplByJob(empl_count_per_job):
+
+    df = pd.DataFrame(list(empl_count_per_job.items()), columns=['JobRole', 'EmplCount'])
+    squarify.plot(sizes=df['EmplCount'], label=df['JobRole'], alpha=.6 )
+    plt.axis('off')
 
     buffer = BytesIO()
     plt.savefig(buffer, format='png')
--- StaffTurnoverApp/views.py
+++ StaffTurnoverApp/views.py
@@ -8,55 +8,58 @@ def index(request):
 
 def saveUserInfo(request):
     if request.method == 'POST':
-        user_info = UserInfo()
-        user_info.email = request.POST['email']
-        user_info.companyName = request.POST['organization']
-        user_info.save()
-        file_reader = TextIOWrapper(request.FILES['employee_data'].file, encoding=request.encoding)
-        reader = csv.reader(file_reader)
-        emp_data= EmplData()
-        # emp_data.userId =
-        next(reader, None)
-        for row in reader:
-                empl_data = EmplData.objects.create(
-                    Age=row[0],
-                    Attrition=row[1],
-                    BusinessTravel=row[2],
-                    DailyRate=row[3],
-                    Department=row[4],
-                    DistanceFromHome=row[5],
-                    Education=row[6],
-                    EducationField=row[7],
-                    EmployeeCount=row[8],
-                    EmployeeNumber=row[9],
-                    EnvironmentSatisfaction=row[10],
-                    Gender=row[11],
-                    HourlyRate=row[12],
-                    JobInvolvement=row[13],
-                    JobLevel=row[14],
-                    JobRole=row[15],
-                    JobSatisfaction=row[16],
-                    MaritalStatus=row[17],
-                    MonthlyIncome=row[18],
-                    MonthlyRate=row[19],
-                    NumCompaniesWorked=row[20],
-                    Over18=row[21],
-                    OverTime=row[22],
-                    PercentSalaryHike=row[23],
-                    PerformanceRating=row[24],
-                    RelationshipSatisfaction=row[25],
-                    StandardHours=row[26],
-                    StockOptionLevel=row[27],
-                    TotalWorkingYears=row[28],
-                    TrainingTimesLastYear=row[29],
-                    WorkLifeBalance=row[30],
-                    YearsAtCompany=row[31],
-                    YearsInCurrentRole=row[32],
-                    YearsSinceLastPromotion=row[33],
-                    YearsWithCurrManager=row[34],
-                    userId=UserInfo.objects.latest('id')
-                )
+        # user_info = UserInfo()
+        # user_info.email = request.POST['email']
+        # user_info.companyName = request.POST['organization']
+        # user_info.save()
+        # file_reader = TextIOWrapper(request.FILES['employee_data'].file, encoding=request.encoding)
+        # reader = csv.reader(file_reader)
+        # next(reader, None)
+        # graphic = staffTurnoverResult(reader)
+
+        emp_data_csv = request.FILES['employee_data']
+        emp_data_dataframe = pd.read_csv(emp_data_csv)
+        graphic = staffTurnoverResult(emp_data_dataframe)
+        # emp_data= EmplData()
+        # for row in reader:
+        #         empl_data = EmplData.objects.create(
+        #             Age=row[0],
+        #             Attrition=row[1],
+        #             BusinessTravel=row[2],
+        #             DailyRate=row[3],
+        #             Department=row[4],
+        #             DistanceFromHome=row[5],
+        #             Education=row[6],
+        #             EducationField=row[7],
+        #             EmployeeCount=row[8],
+        #             EmployeeNumber=row[9],
+        #             EnvironmentSatisfaction=row[10],
+        #             Gender=row[11],
+        #             HourlyRate=row[12],
+        #             JobInvolvement=row[13],
+        #             JobLevel=row[14],
+        #             JobRole=row[15],
+        #             JobSatisfaction=row[16],
+        #             MaritalStatus=row[17],
+        #             MonthlyIncome=row[18],
+        #             MonthlyRate=row[19],
+        #             NumCompaniesWorked=row[20],
+        #             Over18=row[21],
+        #             OverTime=row[22],
+        #             PercentSalaryHike=row[23],
+        #             PerformanceRating=row[24],
+        #             RelationshipSatisfaction=row[25],
+        #             StandardHours=row[26],
+        #             StockOptionLevel=row[27],
+        #             TotalWorkingYears=row[28],
+        #             TrainingTimesLastYear=row[29],
+        #             WorkLifeBalance=row[30],
+        #             YearsAtCompany=row[31],
+        #             YearsInCurrentRole=row[32],
+        #             YearsSinceLastPromotion=row[33],
+        #             YearsWithCurrManager=row[34],
+        #             userId=UserInfo.objects.latest('id')
+        #         )
                 # print(row[0],row[1],row[2])
 
-    graphic = staffTurnoverResult(reader)
     return render(request, 'prediction_dashboard.html', {'graphic':graphic})
